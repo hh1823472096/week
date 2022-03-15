4f0eac35ffc47b4fa8a17d29120dac3de7262d73
黄宏 <1823472096@qq,con>
2022-03-15 19:16:11 +0800

    week3

--- src/main/java/week3/demo/RegisterServlet.java
+++ src/main/java/week3/demo/RegisterServlet.java
@@ -14,7 +14,7 @@ public class RegisterServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username= request.getParameter("username");
+        String username= request.getParameter("username");//username
         String password= request.getParameter("password");
         String email= request.getParameter("email");
         String gender= request.getParameter("gender");
